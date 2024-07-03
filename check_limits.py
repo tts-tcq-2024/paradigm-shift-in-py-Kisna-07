11ca41e1c6e0915bc161b7dd8e0830306d367b97
Krushna Borse <92684634+Kisna-07@users.noreply.github.com>
2024-07-03 12:10:41 +0530

    separate functions for each conditions

--- check_limits.py
+++ check_limits.py
@@ -1,16 +1,27 @@
 
-def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
+def temperature_in_range(temperature):
+    if temperature < 0 or temperature > 45:
+        print('Temperature is out of range!')
+        return False
+    return True
 
-  return True
+def soc_in_range(soc):
+    if soc < 20 or soc > 80:
+        print('State of Charge is out of range!')
+        return False
+    return True
+
+def charge_rate_in_range(charge_rate):
+    if charge_rate > 0.8:
+        print('Charge rate is out of range!')
+        return False
+    return True
+
+def battery_is_ok(temperature, soc, charge_rate):
+    return (is_temperature_in_range(temperature) and
+            is_soc_in_range(soc) and
+            is_charge_rate_in_range(charge_rate))
+
 
 
 if __name__ == '__main__':
