15d0f0d40b7c11b8140db5e3e00b2dd263b64b42
Krushna Borse <92684634+Kisna-07@users.noreply.github.com>
2024-07-08 10:10:37 +0530

    Wrapped print in another function

--- check_limits.py
+++ check_limits.py
@@ -1,19 +1,21 @@
+def print_error(parameter):
+    print(f'{parameter} is out of range!')
 
 def temperature_in_range(temperature):
     if temperature < 0 or temperature > 45:
-        print('Temperature is out of range!')
+        print_error('Temperature')
         return False
     return True
 
 def soc_in_range(soc):
     if soc < 20 or soc > 80:
-        print('State of Charge is out of range!')
+        print_error('State of Charge')
         return False
     return True
 
 def charge_rate_in_range(charge_rate):
     if charge_rate > 0.8:
-        print('Charge rate is out of range!')
+        print_error('Charge rate')
         return False
     return True
 
